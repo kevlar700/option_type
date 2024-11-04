059ccf26c62a54416ff4be156887ba4125d8c20d
Kevin Chadwick <kc-github@chadwicks.me.uk>
2024-11-04 16:46:29 +0000

    Usability improvements

--- option.ads
+++ option.ads
@@ -1,9 +1,9 @@
 generic
-   type Status_Code is (<>);
+   type Status_Code is limited prrivate;
    type Item is limited private;
 package Option is
 
-   type Result (Output_Available : Boolean := False) is record
+   type Result (Output_Available : Boolean := True) is record
       Status : Status_Code;
       case Output_Available is
          when True =>
