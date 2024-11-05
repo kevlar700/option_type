25f14a2dc2daf8f2d578e057c5795aa73d9ba87f
Kevin Chadwick <kc-github@chadwicks.me.uk>
2024-11-05 13:23:08 +0000

    Update option.ads
    If the Status_Code is a separate out parameter then GNAT will warn you when it isn't checked.

--- option.ads
+++ option.ads
@@ -1,10 +1,12 @@
 generic
-   type Status_Code is limited private;
+   --  If the Status_Code is a separate out parameter then GNAT will
+   --  warn you when it isn't checked. 
+   --  type Status_Code is limited private;
    type Item is limited private;
 package Option is
 
    type Result (Output_Available : Boolean := True) is record
-      Status : Status_Code;
+      --  Status : Status_Code;
       case Output_Available is
          when True =>
             Output : Item;
